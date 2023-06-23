25e4982c86b9fdf5f3518e33b05f419048bce88f
asemElkhouli <asemelkhouli209@gmail.com>
2023-06-23 18:06:53 +0300

    Initial Commit

---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by Asem on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
